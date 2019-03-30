a661793a858df126e7e0a88c55bbb408b91a868a
zhangsan <1062906102@qq.com>
2019-03-30 08:26:25 +0800

    service RpcGoodsService update goodsquanlityvalue and mapper update2

--- supermall-common/supermall-common-service/src/main/java/com/zb/service/RpcGoodsService.java
+++ supermall-common/supermall-common-service/src/main/java/com/zb/service/RpcGoodsService.java
@@ -1,12 +1,13 @@
 package com.zb.service;
 
 import com.zb.pojo.Goods;
+import com.zb.pojo.Goodsqualityvalue;
 
 import java.util.List;
 import java.util.Map;
 
 public interface RpcGoodsService {
-    public List<Goods> getGoodsListByMap(Map<String,Object> param)throws Exception;
+    public List<Goodsqualityvalue> getGoodsListByMap(Map<String,Object> param)throws Exception;
 
     public Integer getGoodsCountByMap(Map<String,Object> param)throws Exception;
 }
